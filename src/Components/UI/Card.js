8a51a8f96a922695f2ab5b68f2cc60d96671e38e
Jetch123 <jetchells1995@gmail.com>
2023-06-01 19:04:58 +0200

    Added features

---
+++ src/Components/UI/Card.js
@@ -0,0 +1,8 @@
+import "./Card.css";
+
+const Card = (props) => {
+  const Classes = "card " + props.className;
+  return <div className={Classes}>{props.children}</div>;
+};
+
+export default Card;
